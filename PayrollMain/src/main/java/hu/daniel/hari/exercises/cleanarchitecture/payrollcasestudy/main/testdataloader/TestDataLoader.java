5b6b0d6fccd022a92a7257eb74ceab16d364eb75
Hári Dániel <haridaniel0@gmail.com>
2016-05-20 18:43:45 +0200

    TestData -> english

--- PayrollMain/src/main/java/hu/daniel/hari/exercises/cleanarchitecture/payrollcasestudy/main/testdataloader/TestDataLoader.java
+++ PayrollMain/src/main/java/hu/daniel/hari/exercises/cleanarchitecture/payrollcasestudy/main/testdataloader/TestDataLoader.java
@@ -21,10 +21,10 @@ public class TestDataLoader {
 	}
 
 	private void insertTestEmployees(UseCaseFactoriesAll useCaseFactories) {
-		useCaseFactories.addSalariedEmployeeUseCase().execute(new AddSalariedEmployeeRequest(1, "Kovács Pista", "Vác, Damjanich u. 1.", 3000));
-		useCaseFactories.addHourlyEmployeeUseCase().execute(new AddHourlyEmployeeRequest(2, "Pandacsöki Boborján", "Budapest XI.", 21));
-		useCaseFactories.addHourlyEmployeeUseCase().execute(new AddHourlyEmployeeRequest(3, "Telki Zoltán", "Budapest Ovari u.", 25));
-		useCaseFactories.addCommissionedEmployeeUseCase().execute(new AddCommissionedEmployeeRequest(5, "Takarékos Renáta", "Mende, Gyömrői út 2", 1650, 0.15d));
+		useCaseFactories.addSalariedEmployeeUseCase().execute(new AddSalariedEmployeeRequest(1, "Stuart Dueton", "10 Rue Bailleul, Paris", 3000));
+		useCaseFactories.addHourlyEmployeeUseCase().execute(new AddHourlyEmployeeRequest(2, "Stéphane Toudret", "7 Rue du Joinville 75019, Paris", 21));
+		useCaseFactories.addHourlyEmployeeUseCase().execute(new AddHourlyEmployeeRequest(3, "John Merceland", "Ovari u. 45, 1092 Budapest", 25));
+		useCaseFactories.addCommissionedEmployeeUseCase().execute(new AddCommissionedEmployeeRequest(5, "Jean-Paul Pham", "23 Rue de Crimée, Paris", 1650, 0.15d));
 
 		//Paymentmethods
 		useCaseFactories.changeToDirectPaymentMethodUseCase().execute(new ChangeToDirectPaymentMethodRequest(1, "16200223-10041865"));
